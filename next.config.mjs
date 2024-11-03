dffea57cb30219f96ea35ab64067714530f1b849
SammuelChang <s23593517844@hotmail.com>
2024-11-03 13:35:05 +0800

    chore: disable check during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
     // (Optional) Export as a standalone site
     // See https://nextjs.org/docs/pages/api-reference/next-config-js/output#automatically-copying-traced-files
     output: 'standalone', // Feel free to modify/remove this option
